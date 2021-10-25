e740565a05d8dd521fa5f9b94866323571fe560c
chenghao <chenghao@xkw.cn>
2021-10-25 10:52:13 +0800

    423

--- flutter/demo1/lib/main.dart
+++ flutter/demo1/lib/main.dart
@@ -1,13 +1,85 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  // num a = 1.5;
-  // print(a);
-  // a = 1;
-  // print(a);
-  // numDemo();
-  // listDemo();
-  mapDemo();
+// void main() {
+//   // num a = 1.5;
+//   // print(a);
+//   // a = 1;
+//   // print(a);
+//   // numDemo();
+//   // listDemo();
+//   // mapDemo();
+//   // operatorDemo();
+//   functionDemo();
+// }
+
+void main() => functionDemo();
+
+/**
+ * Dart中的方法
+ * 方法也是一个对象
+ * 返回值和参数类型可以省略
+ * 当方法的执行语句只有一句的时候，可以使用箭头函数 => 表达式
+ * 定义可选参数必须带上形参的名字和类型
+ */
+void functionDemo() {
+  // print(sum(1, 20));
+  // print(sum1(1,c: 9,b: 109));
+  // print(sum2(1,3,9));
+
+  var list = [1,2,3,4];
+  list.forEach(printElement);
+
+  var hello = printHello;
+}
+
+forEachDemo(List list, void func(var e)) {
+  for(var e in list) func(e);
+}
+
+int b = 0;
+
+printElement(var a) {
+  b++;
+  print("元素$b = $a");
+}
+
+printHello() => print("hello");
+
+// int sum(int a, int b) {
+//   return a + b;
+// }
+sum(a, b) => a==1? a + b : a - b;
+
+// sum1(int a, {var b, var c}) {
+//   b ??= 0;
+//   c ??= 0;
+//   return a + b + c;
+// }
+
+//?表示可以为空? 告诉编译器，我处理了后面为空的情况
+// sum1(int a, {int? b, int? c}) {
+//   b ??= 0;
+//   c ??= 0;
+//   return a + b + c;
+// }
+
+sum1(int a, {int b = 0, int c = 0}) {
+  return a + b + c;
+}
+
+int sum2(int a,[int b = 0, int c = 0]) {
+  return a + b + c;
+}
+
+void operatorDemo() {
+  var a;
+  a ??= 10;//如果a为空则赋值10，如果不为空则不赋值
+  a ??= 5;
+  print(a);
+
+  var b;
+  b = 5;
+  print(b ?? a);//如果左边有值则返回左边，没有就返回右边
 }
 
 /**
