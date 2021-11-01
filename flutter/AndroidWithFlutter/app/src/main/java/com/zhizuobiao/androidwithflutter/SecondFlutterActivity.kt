9dc2ec723331d5beabed71f720a0889beeaec819
chenghao <chenghao@xkw.cn>
2021-11-01 16:26:58 +0800

    AndroidWithFlutter

---
+++ flutter/AndroidWithFlutter/app/src/main/java/com/zhizuobiao/androidwithflutter/SecondFlutterActivity.kt
@@ -0,0 +1,12 @@
+package com.zhizuobiao.androidwithflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+/**
+ * @program: AndroidWithFlutter
+ * @description:
+ * @author: chenghao
+ * @date: 2021-11-01 11:06
+ **/
+class SecondFlutterActivity : FlutterActivity() {
+}
