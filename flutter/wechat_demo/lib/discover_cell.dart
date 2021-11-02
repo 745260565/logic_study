8d0996c0757450f762a32ac1777725b6b8467039
chenghao <chenghao@xkw.cn>
2021-11-02 13:47:39 +0800

    123

--- flutter/wechat_demo/lib/discover_cell.dart
+++ flutter/wechat_demo/lib/discover_cell.dart
@@ -2,17 +2,23 @@ import 'package:flutter/material.dart';
 
 class DiscoverCell extends StatelessWidget {
 
-  final String title;
-  final String imageName;
-  final String subTitle;
-  final String subImageName;
+  final String? title;
+  final String? imageName;
+  final String? subTitle;
+  final String? subImageName;
+
+  DiscoverCell({
+    required this.title,
+    this.subTitle,
+    required this.imageName,
+    this.subImageName,}) : assert(title != null,'title不能为空！'),assert(imageName != null,'imageName不能为空！');
 
 
-  DiscoverCell({this.title, this.imageName, this.subTitle, this.subImageName});
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      color: Colors.white,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
@@ -22,22 +28,22 @@ class DiscoverCell extends StatelessWidget {
             child: Row(
               children: [
                 //图标
-                Image(image: AssetImage(imageName),width: 20,),
-                SizedBox(width: 15,),
+                Image(image: AssetImage(imageName!),width: 20,),
+                const SizedBox(width: 15,),
                 //Title
-                Text(title),
+                Text(title!),
               ],
             ),
           ),
           //right
           Container(
-            padding: EdgeInsets.all(10),
+            padding: const EdgeInsets.all(10),
             child: Row(
               children: [
                 //subtitle
-                subTitle != null ? Text(subTitle) : Text(""),
+                subTitle != null ? Text(subTitle!) : const Text(""),
                 //subImage
-                subImageName != null ? Image.asset(subImageName) : Container(),
+                subImageName != null ? Image.asset(subImageName!) : Container(),
                 //箭头
                 Image(image: AssetImage("images/icon_right.png"),width: 15,)
               ],
--- flutter/wechat_demo/lib/discover_page.dart
+++ flutter/wechat_demo/lib/discover_page.dart
@@ -26,7 +26,39 @@ class _DiscoverPageState extends State<DiscoverPage> {
         height: 800,
         color: _themColor,
         child: ListView(
-          children: [
+          children: <Widget>[
+            DiscoverCell(imageName: 'images/朋友圈.png', title: '朋友圈',),
+            SizedBox(height: 10,),
+            DiscoverCell(imageName: 'images/扫一扫2.png', title: '扫一扫',),
+            Row(
+              children: <Widget>[
+                Container(width: 50,height: 0.5,color: Colors.white,),
+                Container(height: 0.5,color: Colors.grey,),
+              ],
+            ),
+            DiscoverCell(title: '摇一摇', imageName: 'images/摇一摇.png'),
+            SizedBox(height: 10,),
+            DiscoverCell(title: '看一看', imageName: 'images/看一看icon.png'),
+            Row(
+              children: <Widget>[
+                Container(width: 50,height: 0.5,color: Colors.white,),
+                Container(height: 0.5,color: Colors.grey,),
+              ],
+            ),
+            DiscoverCell(imageName: 'images/搜一搜 2.png', title: '搜一搜',),
+            SizedBox(height: 10,),
+            DiscoverCell(imageName: 'images/附近的人icon.png', title: '附近的人',),
+            SizedBox(height: 10,),
+            DiscoverCell(title: '购物', imageName: 'images/购物.png',subTitle: '618限时特价',subImageName: 'images/badge.png',),
+            Row(
+              children: <Widget>[
+                Container(width: 50,height: 0.5,color: Colors.white,),
+                Container(height: 0.5,color: Colors.grey,),
+              ],
+            ),
+            DiscoverCell(title: '游戏', imageName: 'images/游戏.png'),
+            SizedBox(height: 10,),
+            DiscoverCell(title: '小程序', imageName: 'images/小程序.png'),
           ],
         ),
       ),
