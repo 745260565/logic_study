a98f867d2ccc1af18caba407d3ddadb909d8a980
chenghao <chenghao@xkw.cn>
2021-11-30 10:37:26 +0800

    123

---
+++ flutter/native_flutter/NativeDemo/NativeDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NativeDemo
+//
+//  Created by ChengHao on 2021/11/29.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
