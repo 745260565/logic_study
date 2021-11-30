943a1b75b904974bc36430079655eaa730398de2
chenghao <chenghao@xkw.cn>
2021-11-30 15:54:38 +0800

    123

--- flutter/flutter_engine/ios/Runner/AppDelegate.m
+++ flutter/flutter_engine/ios/Runner/AppDelegate.m
@@ -1,21 +1,18 @@
 #import "AppDelegate.h"
 #import "GeneratedPluginRegistrant.h"
 
-@interface AppDelegate()
-@property(nonatomic, strong) FlutterEngine *flutterEngine;
-@property(nonatomic, strong) FlutterViewController *flutterVc;
-@property(nonatomic, strong) FlutterMethodChannel *methodChannel;
-
-@end
-
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
-//    self.flutterVc = [[FlutterViewController alloc] initWithEngine:self.flutterEngine nibName:nil bundle:nil];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    NSLog(@"123");
+}
+
 @end
+
