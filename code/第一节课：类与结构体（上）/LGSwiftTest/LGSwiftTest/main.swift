074614cc0e43eb87cbed0330407012ec7a71ed6e
chenghao <chenghao@xkw.cn>
2022-01-07 22:53:23 +0800

    123

--- code/第一节课：类与结构体（上）/LGSwiftTest/LGSwiftTest/main.swift
+++ code/第一节课：类与结构体（上）/LGSwiftTest/LGSwiftTest/main.swift
@@ -25,14 +25,104 @@ import Foundation
 //    var name = "ch"
 //}
 
-struct Teacher{
-    var age: Int = 18
-    var name: String = "ch"
+//struct Teacher{
+//    var age: Int = 18
+//    var name: String = "ch"
+//}
+//
+//var size: UInt = 0
+//var ptr = getsectdata("__TEXT", "", &size)
+//var mHeaderPtr = _dyld_get_image_header(0)
+
+
+
+
+
+//var size: UInt = 0
+////__swift5_types section 的pFile
+//var ptr = getsectdata("__TEXT", "__swift5_types", &size)
+////print(ptr)
+////获取当前程序运行地址 0x0000000100000000
+//var mhHeaderPtr = _dyld_get_image_header(0)
+//var setCommond64Ptr = getsegbyname("__LINKEDIT")
+//var linkBaseAddress: UInt64 = 0
+//if let vmaddr = setCommond64Ptr?.pointee.vmaddr, let fileOff = setCommond64Ptr?.pointee.fileoff{
+//    linkBaseAddress = vmaddr - fileOff
+//}
+//
+//var offset: UInt64 = 0
+//if let unwrappedPtr = ptr{
+//    let intRepresentation = UInt64(bitPattern: Int64(Int(bitPattern: unwrappedPtr)))
+//    offset = intRepresentation - linkBaseAddress
+//}
+//
+////DataLo的内存地址
+//let mhHeaderPtr_IntRepresentation = UInt64(bitPattern: Int64(Int(bitPattern: mhHeaderPtr)))
+//
+//var dataLoAddress = mhHeaderPtr_IntRepresentation + offset
+////print(UnsafePointer<UInt32>.init(bitPattern: Int(exactly: dataLoAddress) ?? 0)?.pointee)
+//
+//var dataLoAddressPtr = withUnsafePointer(to: &dataLoAddress){return $0}
+////print(dataLoAddressPtr)
+//
+//var dataLoContent = UnsafePointer<UInt32>.init(bitPattern: Int(exactly: dataLoAddress) ?? 0)?.pointee
+//
+//let typeDescOffset = UInt64(dataLoContent!) + offset - linkBaseAddress
+//
+//var typeDescAddress = typeDescOffset + mhHeaderPtr_IntRepresentation
+//
+////print(typeDescAddress)
+//struct TargetClassDescriptor{
+//    var flags: UInt32
+//    var parent: UInt32
+//    var name: Int32
+//    var accessFunctionPointer: Int32
+//    var fieldDescriptor: Int32
+//    var superClassType: Int32
+//    var metadataNegativeSizeInWords: UInt32
+//    var metadataPositiveSizeInWords: UInt32
+//    var numImmediateMembers: UInt32
+//    var numFields: UInt32
+//    var fieldOffsetVectorOffset: UInt32
+//    var Offset: UInt32
+//    var size: UInt32
+//}
+
+
+//枚举
+//关联值
+//enum Shap{
+//    case circle(radious: Double)
+//    case rectangle(width: Double, height: Double)
+//}
+//
+//var circle = Shap.circle(radious: 10.0)
+
+enum Enum{
+    case one(Bool)
+    case two
+    case three
+    case four
+    case five
+    case six
+    case four_1
+    case five_2
+    case six_1
+    case two_1
+    case three_1
+    case four_2
+    case five_3
+    case six_2
+    case four_3
+    case five_4
+    case six_4
 }
 
-var size: UInt = 0
-var ptr = getsectdata("__TEXT", "", &size)
-var mHeaderPtr = _dyld_get_image_header(0)
+print(MemoryLayout<Enum>.size)
+print(MemoryLayout<Enum>.stride)
+
+//以最大关联值来开辟内存空间
+
 
 
 
--- iOS/SwiftDemo/SwiftDemo/UI/Swift/PointerViewController.swift
+++ iOS/SwiftDemo/SwiftDemo/UI/Swift/PointerViewController.swift
@@ -48,12 +48,77 @@ class PointerViewController: UIViewController {
 //
 //        tPtr.advanced(by: 0).initialize(to: <#T##PointStruct#>)
         
+//        machoTest()
         
+    }
+    
+    func machoTest(){
         //指针读取Macho中的属性名称
         var size: UInt = 0
         var ptr = getsectdata("__TEXT","__swift5_types", &size)
-        print(ptr)
+        //获取当前程序运行的地址
+        //得到Macho header的基地址
+        var mhHeaderPtr = _dyld_get_image_header(0)
+        //获取
+        var setCommon64Ptr = getsegbyname("__LINKEDIT")
+        //计算当前链接的基地址
+        var linkBaseAddress: UInt64 = 0
+        if let vmaddr = setCommon64Ptr?.pointee.vmaddr, let fileOff = setCommon64Ptr?.pointee.fileoff{
+            linkBaseAddress = vmaddr - fileOff
+        }
+        
+        //内存地址转换成UInt64位置
+        var offset: UInt64 = 0
+        if let unwrappedPtr = ptr{
+            let intRepresentation = UInt64(bitPattern: Int64(Int(bitPattern: unwrappedPtr)))
+            offset = intRepresentation - linkBaseAddress
+            print(offset)
+        }
+        
+        //DataLo的内存地址
+        //将当前程序运行的首地址转成UInt64，便于后续计算
+        let mhHeaderPtr_IntRepresentation = UInt64(bitPattern: Int64(Int(bitPattern: mhHeaderPtr)))
+        var dataLoAddress = mhHeaderPtr_IntRepresentation + offset
+
+        var dataLoAddressPtr = withUnsafePointer(to: &dataLoAddress) {
+            return $0
+        }
+
+        var dataLoContent = UnsafePointer<UInt32>.init(bitPattern: Int(exactly: dataLoAddress) ?? 0)?.pointee
+        print(dataLoContent)
+
+        let typeDescOffset = UInt64(dataLoContent!) + offset - linkBaseAddress
+
+        var typeDecAddress = typeDescOffset + mhHeaderPtr_IntRepresentation
+
+        let classDesriptor = UnsafePointer<TargetClassDescriptor>.init(bitPattern: Int(exactly: typeDecAddress) ?? 0)?.pointee
+
+        if let name = classDesriptor?.name {
+            let nameOffset = Int64(name) + Int64(typeDescOffset) + 8
+            print(nameOffset)
+            let nameAddress = nameOffset + Int64(mhHeaderPtr_IntRepresentation)
+            print(nameAddress)
+            if let cChar = UnsafePointer<CChar>.init(bitPattern: Int(nameAddress)){
+                print(String(cString: cChar))
+            }
+        }
     }
+}
+
+struct TargetClassDescriptor{
+    var flags: UInt32
+    var parent: UInt32
+    var name: Int32
+    var accessFunctionPointer: Int32
+    var fieldDescriptor: Int32
+    var superClassType: Int32
+    var metadataNegativeSizeInWords: UInt32
+    var metadataPositiveSizeInWords: UInt32
+    var numImmediateMembers: UInt32
+    var numFields: UInt32
+    var fieldOffsetVectorOffset: UInt32
+    var Offset: UInt32
+    var size: UInt32
 }
 
 struct PointClass {
